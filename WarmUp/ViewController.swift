8f0c8f49b1f4184c9c739213a64f7f37bee0a62c
Carlos Memije <carlosmemije25@gmail.com>
2022-03-30 19:03:24 -0700

    Starting files

---
+++ WarmUp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WarmUp
+//
+//  Created by Carlos Memije on 3/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
